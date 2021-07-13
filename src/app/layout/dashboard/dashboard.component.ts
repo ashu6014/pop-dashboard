21eefd9eb3c6d4e8ad4e503275cc7be8d093da49
ashu6014 <teegullaaishwarya@gmail.com>
2021-07-13 14:05:52 -0500

    activity structure done

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,25 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
 import { single } from './data';
 import * as XLSX from 'xlsx';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' }
-];
 
 @Component({
     selector: 'app-dashboard',
@@ -28,55 +10,43 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class DashboardComponent implements OnInit {
 
-    
-
-    displayedColumns = ['position', 'name', 'weight', 'symbol'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA);
-    places: Array<any> = [];
-
-    applyFilter(filterValue: string) {
-        filterValue = filterValue.trim(); // Remove whitespace
-        filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-        this.dataSource.filter = filterValue;
-    }
-
-    constructor(private http: HttpClient) {
-       //was here
-        this.places = [
-            {
-                imgSrc: 'assets/images/card-1.jpg',
-                place: 'Cozy 5 Stars Apartment',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.',
-                charge: '$899/night',
-                location: 'Barcelona, Spain'
-            },
-            {
-                imgSrc: 'assets/images/card-2.jpg',
-                place: 'Office Studio',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.',
-                charge: '$1,119/night',
-                location: 'London, UK'
-            },
-            {
-                imgSrc: 'assets/images/card-3.jpg',
-                place: 'Beautiful Castle',
-                description:
-                    // tslint:disable-next-line:max-line-length
-                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.',
-                charge: '$459/night',
-                location: 'Milan, Italy'
-            }
-        ];
-    }
+    constructor(private http: HttpClient) {}
+    single: any[];
+    activityStructureFinal: any[];
 
     ngOnInit() {
-        let activityStructureFinal = [];
-    
-        let sheetDataJson;
+        this.activityStructure();
+    }
+
+    // options
+    view: any[] = [700, 400];
+    showXAxis = true;
+    showYAxis = true;
+    gradient = false;
+    showLegend = false;
+    showXAxisLabel = true;
+    xAxisLabel = 'Activity Structure';
+    showYAxisLabel = true;
+    yAxisLabel = 'Percentage';
+
+    colorScheme = {
+         domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    };
+
+  categories: Category[] = [
+    {value: 'all', viewValue: 'All Categories'},
+    {value: 'eslStrategy', viewValue: 'ESL Strategy'},
+    {value: 'curriculum', viewValue: 'Curriculum'},
+    {value: 'physicalGroup', viewValue: 'Physical Group'},
+    {value: 'activityStructure', viewValue: 'Activity structure'},
+    {value: 'communicationMode', viewValue: 'Communication Mode'},
+    {value: 'languageContent', viewValue: 'Language Content'},
+    {value: 'languageOfInstruction', viewValue: 'Language of Instruction'}
+  ];
+
+  selectedCategory = this.categories[0].value;
+
+  activityStructure(){
         this.http.get('./assets/sample.xlsx', { responseType: 'arraybuffer' }).subscribe((file:ArrayBuffer)  => { 
         let data = new Uint8Array(file);
         var arr = new Array();    
@@ -85,7 +55,7 @@ export class DashboardComponent implements OnInit {
         let workbook = XLSX.read(bstr, {type:"binary"});
         let first_sheet_name = workbook.SheetNames[0];    
         let worksheet = workbook.Sheets[first_sheet_name];    
-        sheetDataJson = XLSX.utils.sheet_to_json(worksheet,{raw:true})
+        let sheetDataJson = XLSX.utils.sheet_to_json(worksheet,{raw:true})
         let asMap = new Map();
         sheetDataJson.forEach(element => {
             if(!asMap.has(element["Activity Structure"])){
@@ -104,65 +74,22 @@ export class DashboardComponent implements OnInit {
             [17, "NA/feed"], [18, "NA/tran"], [19, "NA/int"], [20, "NA/out"],
             [21 , "interact"]
         ]);
+        let testList = [];
         asMap.forEach((value:number, key:number)=> {
             if(value>0){
                 let item = {
                     name: activityStructureMap.get(key),
                     value: Math.round(asMap.get(key)*100/60)
                 };
-                activityStructureFinal.push(item);
+                testList.push(item);
             }   
         });
-        console.log(activityStructureFinal);
-        Object.assign(this, { activityStructureFinal })
+        this.activityStructureFinal = testList
+        console.log(this.activityStructureFinal);
+        Object.assign(this, { testList })
+        console.log(single)
       })
-           
-    }
-
-    single: any[];
-
-  view: any[] = [700, 400];
-
-  // options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Country';
-  showYAxisLabel = true;
-  yAxisLabel = 'Population';
-
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
-
-
-  onSelect(event) {
-    console.log(event);
   }
-
-
-  categories: Category[] = [
-    {value: 'all', viewValue: 'All Categories'},
-    {value: 'eslStrategy', viewValue: 'ESL Strategy'},
-    {value: 'curriculum', viewValue: 'Curriculum'},
-    {value: 'physicalGroup', viewValue: 'Physical Group'},
-    {value: 'activityStructure', viewValue: 'Activity structure'},
-    {value: 'communicationMode', viewValue: 'Communication Mode'},
-    {value: 'languageContent', viewValue: 'Language Content'},
-    {value: 'languageOfInstruction', viewValue: 'Language of Instruction'}
-  ];
-
-  selectedCategory = this.categories[0].value;
-
-  
-
-  
-
-
-
-
 
 }
 
